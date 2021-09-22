2036c93e821194353e782b9a3823afe1f7891f1e
toantran2206 <toantran2206@gmail.com>
2021-09-22 21:58:27 +0700

    Update app1_ace.py

--- app1_ace.py
+++ app1_ace.py
@@ -9,19 +9,43 @@ st.write('With each question, you have 5 choices from "totally disagree" to "tot
 #Answer list:
 choices = ['Totally Disagree','Disagree','Confused','Agree','Totally Agree']
 score_ace = 0
+def calculate(score, question):
+    if question == ('Totally Disagree'):
+        score += 1
+    elif question == ('Disagree'):
+        score += 2
+    elif question == ('Confused'):
+        score += 3
+    elif question == ('Agree'):
+        score += 4
+    else:
+        score += 5 
 
 #Questions
-st.write('1. Tôi biết chọn việc mà làm nên hiếm khi rơi vào trạng thái kiệt sức')
+# st.write('1. Tôi biết chọn việc mà làm nên hiếm khi rơi vào trạng thái kiệt sức')
 q1 = st.radio('1. Tôi biết chọn việc mà làm nên hiếm khi rơi vào trạng thái kiệt sức', options= choices)
 st.write('<style>div.row-widget.stRadio > div{flex-direction:row;} </style>', unsafe_allow_html=True)
-if q1 == ('Totally Disagree'):
-   score_ace += 1
-elif q1 == ('Disagree'):
-    score_ace += 2
-elif q1 == ('Confused'):
-    score_ace += 3
-elif q1 == ('Agree'):
-    score_ace += 4
-else:
-    score_ace += 5 
+# if q1 == ('Totally Disagree'):
+#    score_ace += 1
+# elif q1 == ('Disagree'):
+#     score_ace += 2
+# elif q1 == ('Confused'):
+#     score_ace += 3
+# elif q1 == ('Agree'):
+#     score_ace += 4
+# else:
+#     score_ace += 5 
+calculate(score_ace,q1)
 st.write('Score: ',score_ace)
+q2 = st.radio('2. Khi tôi biết mục tiêu của mình là gì và cần làm gì để đạt được mục tiêu đó thì tôi thường cố gắng làm đến nơi đến chốn:',options=choices)
+st.write('<style>div.row-widget.stRadio > div{flex-direction:row;} </style>', unsafe_allow_html=True)
+# if q2 == ('Totally Disagree'):
+#    score_ace += 1
+# elif q2 == ('Disagree'):
+#     score_ace += 2
+# elif q1 == ('Confused'):
+#     score_ace += 3
+# elif q1 == ('Agree'):
+#     score_ace += 4
+# else:
+#     score_ace += 5 
