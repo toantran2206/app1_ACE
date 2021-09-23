f12340baae225261c037ac6812ed5745f2f30b9d
toantran2206 <toantran2206@gmail.com>
2021-09-23 17:16:12 +0700

    Update app1_ace.py

--- app1_ace.py
+++ app1_ace.py
@@ -174,21 +174,21 @@ def page_third():
     'B':'Tôi thấy dằn vặt nhưng nhanh chóng có lý do phù hợp cho việc đó',
     'C':'Tôi chấp nhận vì điều không trung thực đó trong phạm vi cho phép',
     'D':'Tôi không thấy có vấn đề gì'}
-    q2 = st.radio('2.Khi làm một điều gì đó thiếu trung thực, bạn thường:', options= choice2)
+    q2 = st.radio('2.Khi làm một điều gì đó thiếu trung thực, bạn thường:', options= choice2.values())
     score_scm += cal(score_scm,q2,choice2)
 
     choice3 = {'A':'Tôi thấy tệ về bản thân và quyết tâm không bao giờ làm lại hành vi đó nữa',
     'B':'Tôi thấy khó chịu và tìm lý do để bào chữa',
     'C':'Tôi xem xét lại việc thiếu trung thực và cho rằng nó là ngoại lệ có thể chấp nhận',
     'D':'Tôi thấy không có vấn đề gì'}
-    q3 = st.radio('3.Bạn thường có xu hướng gì sau khi làm một hành vi thiếu trung thực:', options= choice3)
+    q3 = st.radio('3.Bạn thường có xu hướng gì sau khi làm một hành vi thiếu trung thực:', options= choice3.values())
     score_scm += cal(score_scm,q3,choice3)
 
     choice4 = {'A':'Chắc chắn không làm dù lý do gì.',
     'B':'Sẽ đắn đo và tìm lý do thuyết phục để làm việc đó hay không',
     'C':'Sẽ xem lại việc này có nằm trong danh sách cho phép của tôi không',
     'D':'Không cần phải đắn đo gì nhiều'}
-    q4 = st.radio('4.Đứng trước một cơ hội để thiếu trung thực (để có một lợi ích hoặc tránh một nỗi đau) nhưng không bị ai phát hiện, tôi sẽ:', options= choice4)
+    q4 = st.radio('4.Đứng trước một cơ hội để thiếu trung thực (để có một lợi ích hoặc tránh một nỗi đau) nhưng không bị ai phát hiện, tôi sẽ:', options= choice4.values())
     score_scm += cal(score_scm,q4,choice4)
     st.write('Score: ',score_scm)
 
